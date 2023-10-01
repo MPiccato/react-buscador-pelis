a547ef2a6980e2af12cfc53ae8b1b83d690ecb0b
MP Data - Trading <42841276+MPiccato@users.noreply.github.com>
2023-10-01 10:54:20 -0300

    custom hooks search

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,56 @@
 import './App.css';
+
 import { useMovies } from './hooks/useMovies';
 import { Movies } from './components/Movies';
+import { useEffect, useState } from 'react';
+
+function useSearch() {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (search === '') {
+      setError('no se pueden buscar pelis sin nombre')
+      return
+    }
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una peli con un número')
+      return
+    }
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos tres caracteres')
+      return
+    }
+
+    setError(null)
+
+  }, [search])
+
+  return { search, updateSearch, error }
+
+}
+
 
 
 function App() {
 
-  const { movies: mappedMovies } = useMovies()
+  const { movies } = useMovies();
+  const { search, updateSearch, error } = useSearch()
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log({ search })
+  }
+
+  const handleChange = (event) => {
+
+
+    updateSearch(event.target.value)
+
+  }
+
 
 
 
@@ -13,15 +58,19 @@ function App() {
     <div className='page'>
 
       <header>
-        <form className='form'>
-          <input placeholder='Avengers, Star Wars, The Matrix...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            style={{ border: '1px solid blue', borderColor: error ? 'red' : 'blue' }}
+            onChange={handleChange} value={search} name='query'
+            placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
         {/* Peliculas */}
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div >
   )
